92ff8fcd08ab808467e4f3e64c423782a2ec2ca2
hboichuk <annabelfeer@gmail.com>
2023-10-29 12:47:49 +0100

    raw version of Client

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,11 +6,59 @@
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 18:45:02 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/10/23 18:45:47 by hboichuk         ###   ########.fr       */
+/*   Updated: 2023/10/29 12:46:49 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_HPP
 # define CLIENT_HPP
+
+#include "Webserv.hpp"
+//we need Request
+//we need Response
+
+/* Stores all information about Client during Request-Response process*/
+class Client 
+{
+	private:
+		/*keep track of the time when the last message was received or sent by this client*/
+		time_t	_last_msg_time;
+		/*stores the socket descriptor associated with the client's network connection
+		 It allows the server to send and receive data to and from the client */
+		int	_client_fd_socket;
+		/*store information about the client's network address(IP address and port number of the client)*/
+		struct sockaddr_in  _client_ip_and_port;
+
+		 
+	public:
+		Client();
+		Client(ServerConfig &);
+		Client(const Client &other);
+		Client &operator=(const Client & rhs);
+		~Client();
+
+		//Response object
+		//Request object
+		//ServerConfig object
+
+	//getters
+		/*we need & here for incapsulation*/
+		const int                 &getSocket() const;
+        const struct sockaddr_in  &getIp() const;
+        const time_t              &getLastTime() const;
+		//getter for Request
+		
+	//setter
+		void                setSocket(int &);
+        void                setIp(sockaddr_in &);
+        // void                setServer(ServerConfig &);//?
+		
+	//methods
+		//build Response
+		//method update for time
+		//clear Client
+	
+}
+
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -6,8 +6,78 @@
 /*   By: hboichuk <hboichuk@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 18:46:04 by hboichuk          #+#    #+#             */
-/*   Updated: 2023/10/23 18:46:24 by hboichuk         ###   ########.fr       */
+/*   Updated: 2023/10/29 12:37:49 by hboichuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Client.hpp"
+
+Client::Client()
+{
+    _last_msg_time = time(NULL);
+}
+
+Client::Client(const Client &other)
+{
+    if (this != &other)
+    {
+        this->_client_fd_socket = other._client_fd_socket;
+        this->_client_ip_and_port = other._client_ip_and_port;
+        this->_last_msg_time = other._last_msg_time;
+        //response
+        //server config
+        //request
+    }
+    return ;
+}
+
+Client::Client &operator=(const Client & rhs)
+{
+    if (this != &rhs)
+    {
+        this->_client_fd_socket = rhs._client_fd_socket;
+        this->_client_ip_and_port = rhs._client_ip_and_port;
+        this->_last_msg_time = rhs._last_msg_time;
+        //response
+        //server config
+        //request
+    }
+    return (*this);
+}
+
+Client::Client(ServerConfig &server)
+{
+    //doesn't finished
+}
+
+Client::~Client() {}
+
+//methods
+
+//getters
+const int   Client::&getSocket() const
+{
+    return (_client_fd_socket);
+}
+
+const struct sockaddr_in  Client::&getIp() const
+{
+    return (_client_ip_and_port);
+}
+
+const time_t    Client::&getLastTime() const
+{
+    return (_last_msg_time);
+}
+
+//setters
+void    Client::setSocket(int &socket)
+{
+    _client_fd_socket = socket;
+}
+
+void    Client::setIp(sockaddr_in &ip)
+{
+    _client_ip_and_port = ip;
+}
+

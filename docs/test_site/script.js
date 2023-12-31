3df23b9aa87f982c469e3fcdca6f2442494db39c
hboichuk <annabelfeer@gmail.com>
2023-12-31 16:18:33 +0100

    add starter for test site

---
+++ docs/test_site/script.js
@@ -0,0 +1,40 @@
+const textEndpointUrl = '/api/text.txt';
+const fileBaseEndpointUrl = '/api/';
+
+function submitText() {
+    const input = document.getElementById('input').value;
+    sendData(input, textEndpointUrl);
+}
+
+function submitImage() {
+    const fileInput = document.getElementById('image');
+    const file = fileInput.files[0];
+    sendData(file, fileBaseEndpointUrl + encodeURIComponent(file.name));
+}
+
+function sendData(data, endpointUrl) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', endpointUrl, true);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            document.getElementById('output').innerHTML = this.responseText;
+        }
+    };
+
+    if (data instanceof File) {
+        xhr.setRequestHeader('Content-Type', data.type);
+        xhr.setRequestHeader('X-File-Name', data.name);
+        xhr.send(data);
+    } else {
+        xhr.setRequestHeader('Content-Type', 'text/plain');
+        xhr.setRequestHeader('X-File-Name', 'text.txt');
+        xhr.send(data);
+    }
+}
+
+function deleteText() {
+    const textDiv = document.getElementById('textToDelete');
+    const xhr = new XMLHttpRequest();
+    xhr.open('DELETE', '/api/upload/text.txt', true);
+    xhr.send();
+}
